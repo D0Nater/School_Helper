362c0819033ecd9adfd12840a93b456dea3a1afb
D0Nater <53537581+D0Nater@users.noreply.github.com>
2020-12-14 21:07:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,20 @@ import telebot
 
 
 class Main:
-    ADMINS = []
+    ADMINS = [] # your
+
+    VERSION = "0.1"
 
     START_TIME = ""
 
-    TOKEN = ""
+    TOKEN = "" # bot token
 
     BOT = telebot.TeleBot(TOKEN)
 
+    BAN_LIST = []
     USER_LIST = {}
+
+    NEWS =  "за 14.12.2020", \
+            "\nСтарт проекта School Helper.", \
+            "\nС каждым днем количество правил, теорем, а так же решений будет расти!", \
+            "Следите за самыми свежими новостями и оставайтесь в курсе событий!"
